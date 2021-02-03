f7cdf7d7b35f3b084e52bdacf6241dfe320bbb55
t-bolt <isholasobuur@gmail.com>
2021-02-03 18:59:59 +0000

    wrapped th 0 inside a bracket

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+B#include <stdio.h>
 
 /**
    This particular code
@@ -8,5 +8,5 @@
 
 int main(void) {
   puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+  return (0);
 }
