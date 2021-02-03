5eaaf393e5d16a9ee79c16dd837bf3ec394bcef6
t-bolt <isholasobuur@gmail.com>
2021-02-03 18:53:27 +0000

    added void to the main argument

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
    This particular code
    prints the given expression without
    the printf function
-**/
+ **/
 
-int main() {
+int main(void) {
   puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
