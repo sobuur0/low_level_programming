17bef78dd2a88ab7fdc058c484b8e43002cc8a65
t-bolt <isholasobuur@gmail.com>
2021-02-03 19:14:49 +0000

    restructured my code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
-   This particular code
-   prints the given expression without
-   the printf function
- **/
+ * This particular code
+ * prints the given expression without
+ * the printf function
+*/
 
-int main(void) {
+int main(void)
+{
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
