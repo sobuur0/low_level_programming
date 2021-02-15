5bfe75d9a3ee18697aac53a6133954468e93cd82
t-bolt <isholasobuur@gmail.com>
2021-02-15 09:25:04 +0000

    a function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - takes the pointer to an int as parameter and updates the value it points to to 98
+ * @n: pointer
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+  *n = 98;
+}
